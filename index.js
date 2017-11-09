3fb1225b7c9b91f21c54c54f59f10d6fac9f7d0e
Alex Bea <abea@punkave.com>
2017-11-09 13:19:57 -0500

    Makes callback error a warning

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   'extends': 'standard',
   'rules': {
     'semi': ['error', 'always'],
-    'no-unused-vars': ['error', { 'varsIgnorePattern': 'apos', 'args': 'none', 'ignoreRestSiblings': true }]
+    'no-unused-vars': ['error', { 'varsIgnorePattern': 'apos', 'args': 'none', 'ignoreRestSiblings': true }],
+    'standard/no-callback-literal': 'warn'
   }
 };
